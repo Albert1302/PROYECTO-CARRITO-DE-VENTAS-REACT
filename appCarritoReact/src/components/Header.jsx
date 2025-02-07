efee3f6d87185c285abc231625cf8da4a42baf7d
Albert <albertyucracalderon@gmail.com>
2025-02-07 15:40:24 -0400

    CREACION DE LA INTERFAZ DEL CARRITO

---
+++ appCarritoReact/src/components/Header.jsx
@@ -0,0 +1,26 @@
+//rafce
+const Header = () =>{
+
+
+
+    return(
+        <header>
+          
+        <h3>CART REACT </h3>
+        
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Productos</li>
+            <li>Nosotros</li>
+            <li>Contacto</li>
+          </ul>
+          <div>
+
+          </div>
+        </nav>
+      </header>
+    )
+}
+
+export default Header
