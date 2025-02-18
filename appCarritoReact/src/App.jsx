f4f0ad92a1ec688030bf973f0d241afbbbad7f74
Albert <albertyucracalderon@gmail.com>
2025-02-18 15:15:18 -0400

    YA CASI TERMINADO :)

--- appCarritoReact/src/App.jsx
+++ appCarritoReact/src/App.jsx
@@ -1,34 +1,91 @@
 import Header from "./components/Header"
 import Product from "./components/Product"
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import { db } from "./data/db"
 function App() {
-  const [data] = useState(db)
-  const [cart,setCart] = useState([]);
+
+  const initialCart = () =>{
+    const localStoragecart = localStorage.getItem('cart');
+    return localStoragecart ? JSON.parse(localStoragecart ) : []
+  }
+
+  const [data] = useState(db);
+  const [cart,setCart] = useState(initialCart);
+
+  const MAX_ITEMS = 5;
+  const MIN_ITEMS = 1;
+
+  useEffect(()=>{
+    localStorage.setItem('cart',JSON.stringify(cart))
+  },[cart])
 
 
   const agregarProductoCarrito = (item)=> {
     
     const ItemExist = cart.findIndex(product => product.id === item.id );
-    console.log(ItemExist);
-
     if(ItemExist>=0){
+      if(cart[ItemExist].cantidad >= MAX_ITEMS) return;
       const UpdateCart = [...cart];
       UpdateCart[ItemExist].cantidad ++;
       setCart(UpdateCart);
-      console.log("Si existe el producto en el carrito");
     }else{
       item.cantidad = 1;
       setCart([...cart,item]);
     }
     console.log(cart);
-    
   }
+
+
+  const removerProductoCarrito = (id) =>{   
+    const filterCart = cart.filter(product => product.id !== id);
+    setCart(filterCart);
+    //setCart(prevCart => prevCart.filter(product => product.id !== id))
+  }
+
+  const incrementarCantidad = (id) =>{
+    const UpdateCart = cart.map(item =>{
+      if(item.id===id && item.cantidad < MAX_ITEMS){
+        return {
+          ...item,
+          cantidad : item.cantidad + 1
+        }
+      }
+      return item;
+    })
+    setCart(UpdateCart);
+  }
+
+  const decrementarCantidad = (id) =>{
+    const UpdateCart = cart.map(item =>{
+      if(item.id===id && item.cantidad > MIN_ITEMS){
+        return {
+          ...item,
+          cantidad : item.cantidad - 1
+        }
+      }
+      return item;
+    })
+    setCart(UpdateCart);
+  }
+
+  const limpiarCarrito = () =>{
+    setCart([]);
+  }
+
     return (
     <>
-      <Header/>
+      <Header
+        carrito= {cart}   
+        removeFromCart = {removerProductoCarrito}   
+        increaseQuantity = {incrementarCantidad}
+        decreaseQuantity = {decrementarCantidad}
+        clearCart = {limpiarCarrito}
+      />
+
       <main className="mx-auto max-w-4xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h3 className="font-semibold text-blue-blue-600 text-lg">Productos mas vendidos</h3>
+        <h1 className="font-semibold text-blue-blue-600 text-3xl">
+        🚀Productos mas vendidos
+          </h1>
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {
           data.map((product)=>(
--- appCarritoReact/src/components/Header.jsx
+++ appCarritoReact/src/components/Header.jsx
@@ -1,7 +1,12 @@
-//rafce
-const Header = () =>{
+import { useMemo } from "react";
+const Header = ({carrito,removeFromCart,increaseQuantity,decreaseQuantity,clearCart}) =>{
+    // useMemo: Es un hook, que se usa para memorizar valores calculados y evitar calculos innecesarios en cada renderizado
+    // Esto mejora el rendimiento de la aplicacion, especialmente cuando el calculo depende de un estado o cambio frecuente
 
-
+    // const isEmpty = carrito.length === 0;
+    const isEmpty = useMemo(() => carrito.length === 0, [carrito]);
+   // .reduce((acumulador, arreglo )=> 'La funcion que hara',0 'Eso inicia el parametro que se coloca') para sumar los precios de los productos en el carrito
+   const cartTotal = useMemo(()=>carrito.reduce((total,item) => total + (item.cantidad * item.precio),0),[carrito])
 
     return(
       <header className="bg-gray-900 text-white py-4 px-6">
@@ -15,7 +20,79 @@ const Header = () =>{
                   <li className="hover:text-indigo-400 cursor-pointer">Contacto</li>
               </ul>
           </nav>
+          <div className="text-2xl cursor-pointer carrito">
+            🛒
+            <div id="carrito" className="bg-white p-3 text-gray-900">
+                    {
+                        isEmpty ?(
+                            <p>El carrito esta vacio</p>
+                        ):(
+                        // TODA LA TABLA  DE CARRITO DE COMPRAS CREADA
+                        // ALTERNATIVA A UN < DIV >
+                        <>
+                        <table className="w-full table text-base">
+                        <thead className="bg-gray-900">
+                            <tr>
+                                <th className="text-white">Imagen</th>
+                                <th className="text-white">Nombre</th>
+                                <th className="text-white">Precio</th>
+                                <th className="text-white">Cantidad</th>
+                                <th className="text-white">Subtotal</th>
+                                <th className="text-white">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {carrito.map(product => (
+                                 <tr key={product.id}>
+                                 <td>
+                                     <img src={product.imagen} alt="" />
+                                 </td>
+                                 <td>
+                                    {product.nombre}
+                                 </td>
+                                 <td>
+                                    ${product.precio}
+                                 </td>
+                                 <td className="">
+                                    <button onClick={()=>increaseQuantity(product.id)}
+                                    className="px-2 py-1 bg-gray-900 text-white"> + </button>
+                                    {product.cantidad}
+                                    <button onClick={()=>decreaseQuantity(product.id)}
+                                    className="px-2 py-1 bg-gray-900 text-white"> - </button>
+                                 </td>
+                                 <td>
+                                    ${product.precio * product.cantidad}
+                                 </td>
+                                 <td>
+                                    <button onClick={()=>removeFromCart(product.id)} 
+                                    className="bg-red-600 px-2 py-1 cursor-pointer rouded-md"> x </button>
+                                 </td>
+                             </tr>
+                            ))}
+                           
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colSpan="3"></td>
+                                <td colSpan="2"> <p>Total Pagar: $<span>{cartTotal} </span> </p> </td>
+                                <td></td>
+                            </tr>
+                        </tfoot>
+                            </table>
+                            <button onClick={()=>clearCart()} 
+                className="bg-blue-600 w-full mt-3 cursor-pointer hover:bg-red-700 px-1.5 py-1.5 text-white rounded-md"> Vaciar Carrito </button>
+                            </> 
+                        )
+                }  
+               
+
+            </div>
+            </div>
+          {/*Menu movil*/}
+          <div className="md:hidden">
+                <button className="text-white text-2xl">☰</button>
           </div>
+        </div>
       </header>
     )
 }
